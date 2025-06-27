dc262892b19ca4617f4897689be8cfca2337305e
nompilomaqutu <nompilomaqutu@yahoo.com>
2025-06-27 14:04:25 +0200

    Fix Cypress Configuration

--- cypress.config.js
+++ cypress.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config
     },
     env: {
       // Supabase credentials
